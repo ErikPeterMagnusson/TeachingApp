5a552dc7cc0209451172b15f598662fd0e71e337
ErikPeterMagnusson <erikpeterme@gmail.com>
2015-09-23 13:32:40 +0200

    Controllers done

---
+++ TeachingApp/TeachingApp/Controllers/HighscoresController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TeachingApp.DataConnectionlayer;
+using TeachingApp.Models;
+
+namespace TeachingApp.Controllers
+{
+    public class HighscoresController : Controller
+    {
+        private TeachingContext db = new TeachingContext();
+
+        // GET: Highscores
+        public ActionResult Index()
+        {
+            return View(db.Highscore.ToList());
+        }
+
+        // GET: Highscores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Highscore highscore = db.Highscore.Find(id);
+            if (highscore == null)
+            {
+                return HttpNotFound();
+            }
+            return View(highscore);
+        }
+
+        // GET: Highscores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Highscores/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Score")] Highscore highscore)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Highscore.Add(highscore);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(highscore);
+        }
+
+        // GET: Highscores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Highscore highscore = db.Highscore.Find(id);
+            if (highscore == null)
+            {
+                return HttpNotFound();
+            }
+            return View(highscore);
+        }
+
+        // POST: Highscores/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Score")] Highscore highscore)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(highscore).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(highscore);
+        }
+
+        // GET: Highscores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Highscore highscore = db.Highscore.Find(id);
+            if (highscore == null)
+            {
+                return HttpNotFound();
+            }
+            return View(highscore);
+        }
+
+        // POST: Highscores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Highscore highscore = db.Highscore.Find(id);
+            db.Highscore.Remove(highscore);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TeachingApp/TeachingApp/Controllers/SentencesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TeachingApp.DataConnectionlayer;
+using TeachingApp.Models;
+
+namespace TeachingApp.Controllers
+{
+    public class SentencesController : Controller
+    {
+        private TeachingContext db = new TeachingContext();
+
+        // GET: Sentences
+        public ActionResult Index()
+        {
+            return View(db.Sentence.ToList());
+        }
+
+        // GET: Sentences/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sentence sentence = db.Sentence.Find(id);
+            if (sentence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sentence);
+        }
+
+        // GET: Sentences/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sentences/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Text")] Sentence sentence)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sentence.Add(sentence);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sentence);
+        }
+
+        // GET: Sentences/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sentence sentence = db.Sentence.Find(id);
+            if (sentence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sentence);
+        }
+
+        // POST: Sentences/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Text")] Sentence sentence)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sentence).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sentence);
+        }
+
+        // GET: Sentences/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sentence sentence = db.Sentence.Find(id);
+            if (sentence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sentence);
+        }
+
+        // POST: Sentences/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sentence sentence = db.Sentence.Find(id);
+            db.Sentence.Remove(sentence);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
