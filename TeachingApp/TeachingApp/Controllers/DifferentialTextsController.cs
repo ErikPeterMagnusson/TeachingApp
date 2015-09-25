60c90d72a19729ccaf6bcba86ce925fee9ffeb7b
ErikPeterMagnusson <erikpeterme@gmail.com>
2015-09-25 09:46:32 +0200

    testar

--- TeachingApp/TeachingApp/Controllers/DifferentialTextsController.cs
+++ TeachingApp/TeachingApp/Controllers/DifferentialTextsController.cs
@@ -24,11 +24,14 @@ namespace TeachingApp.Controllers
         }
 
         [HttpPost]
-        public ActionResult differentialInput ()
-        {
-            if (repo.CompareDifferentialInput( = true) { Console.WriteLine="true" }
-            Console.WriteLine="false";
-        }
+        //public ActionResult differentialInput ()
+        //{
+            //if (repo.CompareDifferentialInput) 
+            //{
+            //    Console.WriteLine="true" 
+            //}
+            //Console.WriteLine="false";
+        //}
 
         // GET: DifferentialTexts/Details/5
         public ActionResult Details(int? id)
